2745614b04fd79adf0632e636f0cd3aa93f0e083
Jesus Sanchez <jesuss387@gmail.com>
2022-03-30 20:15:24 -0500

    all the changes

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,18 +1,7 @@
-import NavBar from "./Components/NavBar/NavBar";
-import Home from "./Pages/Home/Home";
-import AboutMe from "./Pages/About/AboutMe";
-import Skills from "./Pages/Skills/Skills";
-import Contact from "./Pages/Contact";
-import { Route, Routes } from "react-router-dom";
-
 function App() {
   return (
     <>
-      <NavBar />
-      <Home />
-      <AboutMe />
-      <Skills />
-      <Contact />
+      <h1>Hello</h1>
     </>
   );
 }
--- portfolio/src/Pages/Contact.tsx
+++
@@ -1,9 +0,0 @@
-const Contact = (): JSX.Element => {
-  return (
-    <section id='contact' >
-      <h1>Contact</h1>
-    </section>
-  );
-};
-
-export default Contact;
